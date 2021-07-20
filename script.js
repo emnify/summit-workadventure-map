a6f574aedd24194c9ceb6858693b934f3c17518f
Marina Lang <marina.lang@emnify.com>
2021-07-20 13:18:23 +0200

    Added scripts for Grafana

---
+++ script.js
@@ -0,0 +1,37 @@
+let grafanaPopup;
+
+WA.onEnterZone('jealousguy', () => {
+  WA.sendChatMessage("I'm so jealous right now.", 'Jealous Guy');
+});
+
+grafanaPopup = WA.onEnterZone('dashboard', () => {
+  openPopup('grafanaPopup', "Go to Grafana?", [
+    {
+      label: 'EMnify Prod',
+      className: 'success',
+      callback: (popup) => {
+        WA.openTab('https://grafana.oss-eks.emnify.net');
+        popup.close();
+      },
+    },
+    {
+      label: 'EMnify Dev',
+      className: 'success',
+      callback: (popup) => {
+        WA.openTab('https://grafana.oss-eks.dev.emnify.io/');
+        popup.close();
+      },
+    },
+    {
+      label: 'Not now',
+      className: 'primary',
+      callback: (popup) => {
+        popup.close();
+      },
+    },
+  ]);
+});
+
+WA.onLeaveZone('dashboard', () => {
+  grafanaPopup.close();
+});
