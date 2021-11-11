ef9d0b72ae9c3b0c00219ffb586384face431a24
Marina Lang <marina.lang@emnify.com>
2021-11-11 14:20:48 +0100

    Fix closing of popups

--- custom-script.js
+++ custom-script.js
@@ -1,42 +1,50 @@
 let jealousguyPopup;
 let grafanaPopup;
 
-jealousguyPopup = WA.onEnterZone('jealousguy', () => {
-  WA.openPopup('jealousguyPopup', "I'm so jealous right now.", []);
+WA.onEnterZone('jealousguy', () => {
+  if (!jealousguyPopup) {
+    jealousguyPopup = WA.openPopup('jealousguyPopup', "I'm so jealous right now.", []);
+  }
 });
 
 WA.onLeaveZone('jealousguy', () => {
-  jealousguyPopup.close();
+  if (jealousguyPopup) {
+    jealousguyPopup.close();
+  }
 });
 
-grafanaPopup = WA.onEnterZone('dashboard', () => {
-  WA.openPopup('grafanaPopup', "Go to Grafana?", [
-    {
-      label: 'EMnify Prod',
-      className: 'success',
-      callback: (popup) => {
-        WA.openTab('https://grafana.oss-eks.emnify.net');
-        popup.close();
+WA.onEnterZone('dashboard', () => {
+  if (!grafanaPopup) {
+    grafanaPopup = WA.openPopup('grafanaPopup', "Go to Grafana?", [
+      {
+        label: 'EMnify Prod',
+        className: 'success',
+        callback: (popup) => {
+          WA.openTab('https://grafana.oss-eks.emnify.net');
+          popup.close();
+        },
       },
-    },
-    {
-      label: 'EMnify Dev',
-      className: 'success',
-      callback: (popup) => {
-        WA.openTab('https://grafana.oss-eks.dev.emnify.io/');
-        popup.close();
+      {
+        label: 'EMnify Dev',
+        className: 'success',
+        callback: (popup) => {
+          WA.openTab('https://grafana.oss-eks.dev.emnify.io/');
+          popup.close();
+        },
       },
-    },
-    {
-      label: 'Not now',
-      className: 'primary',
-      callback: (popup) => {
-        popup.close();
+      {
+        label: 'Not now',
+        className: 'primary',
+        callback: (popup) => {
+          popup.close();
+        },
       },
-    },
-  ]);
+    ]);
+  }
 });
 
 WA.onLeaveZone('dashboard', () => {
-  grafanaPopup.close();
+  if (grafanaPopup) {
+    grafanaPopup.close();
+  }
 });
